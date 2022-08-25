cd6c15e5a3942eea50ae23e113156bb4d5ed470a
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2022-08-25 19:31:57 +0300

    csss

--- src/components/Section.js
+++ src/components/Section.js
@@ -8,6 +8,14 @@ function Section() {
         <h1>Model S</h1>
         <p>Order online for Touchless Delivery</p>
     </ItemText>
+    <ButtonGroup>
+        <LeftButton>
+            Custom Order
+        </LeftButton>
+        <RightButton>
+            Existing Inventory
+        </RightButton>
+    </ButtonGroup>
    </Wrap>
   )
 }
@@ -20,9 +28,29 @@ background-size: cover;
 background-position: center;
 background-repeat: no-repeat;
 background-image: url('/images/images/model-s.jpg')
+
 `
 const ItemText = styled.div `
 padding-top: 15vh;
 text-align: center;
 
+`
+const ButtonGroup = styled.div`
+display: flex;
+`
+const LeftButton = styled.div`
+background-color: rgba(23,26,32,0.8);
+height: 40px;
+width: 256px;
+color: white;
+display: flex;
+justify-content: center;
+align-items: center;
+border-radius: 100px;
+opacity: 0.85;
+text-transform: uppercase;
+font-size: 12px;
+
+`
+const RightButton = styled(LeftButton)`
 `
