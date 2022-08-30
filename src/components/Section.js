05c035e36f77f53c9d8a2d66dd2f0d5d67412a83
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2022-08-30 11:13:51 +0300

    test

--- src/components/Section.js
+++ src/components/Section.js
@@ -8,7 +8,7 @@ function Section() {
         <h1>Model S</h1>
         <p>Order online for Touchless Delivery</p>
     </ItemText>
-    <ButtonGroup>
+    <buttonGroup>
         <LeftButton>
             Custom Order
         </LeftButton>
@@ -16,41 +16,51 @@ function Section() {
             Existing Inventory
         </RightButton>
     </ButtonGroup>
+    {/* <DownArrow src="/images/images/down-arrow.svg" /> */}
    </Wrap>
   )
 }
 
 export default Section
-const Wrap = styled.div`
-width: 100vw;
-height: 100vh;
-background-size: cover;
-background-position: center;
-background-repeat: no-repeat;
-background-image: url('/images/images/model-s.jpg')
+// const Wrap = styled.div`
+// width: 100vw;
+// height: 100vh;
+// background-size: cover;
+// background-position: center;
+// background-repeat: no-repeat;
+// background-image: url('/images/images/model-s.jpg')
 
-`
-const ItemText = styled.div `
-padding-top: 15vh;
-text-align: center;
+// `
+// const ItemText = styled.div `
+// padding-top: 15vh;
+// text-align: center;
 
-`
-const ButtonGroup = styled.div`
-display: flex;
-`
-const LeftButton = styled.div`
-background-color: rgba(23,26,32,0.8);
-height: 40px;
-width: 256px;
-color: white;
-display: flex;
-justify-content: center;
-align-items: center;
-border-radius: 100px;
-opacity: 0.85;
-text-transform: uppercase;
-font-size: 12px;
+// `
+// const ButtonGroup = styled.div`
+// display: flex;
+// justify-content: center
+// padding-top: 400px;
 
-`
-const RightButton = styled(LeftButton)`
-`
+// `
+// const LeftButton = styled.div`
+// background-color: rgba(23,26,32,0.8);
+// height: 40px;
+// width: 256px;
+// color: white;
+// display: flex;
+// justify-content: center;
+// align-items: center;
+// border-radius: 100px;
+// opacity: 0.85;
+// text-transform: uppercase;
+// font-size: 12px;
+// cursor: pointer;
+// `
+// const RightButton = styled(LeftButton)`
+// `
+// const DownArrow = styled.img`
+// margin-top: 20px;
+// height: 40px;
+
+
+// `
